d18774c322bf246685865df2b6e043eab40bb123
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-04-09 20:03:22 +0900

    Login page stubs

--- TimeBoxed/Views/LoginView.swift
+++ TimeBoxed/Views/LoginView.swift
@@ -9,13 +9,43 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+
     var body: some View {
-        Text("Username")
+        VStack {
+            Text("Welcome!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            TextField("Login", text: $username)
+                .padding()
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+                .keyboardType(/*@START_MENU_TOKEN@*/.emailAddress/*@END_MENU_TOKEN@*/)
+            SecureField("Password", text: $password)
+                .padding()
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Button(action: Login) {
+                Text("LOGIN")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+            }
+        }
+    }
+
+    func Login() {
+        print("Button tapped")
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        LoginView().previewDevice(PreviewDevice(rawValue: "iPhone SE"))
     }
 }
