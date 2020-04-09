fd8a0e8dca8da2155b9f9c486d9bf92ff9f90903
Paul Traylor <paul.traylor@linecorp.com>
2020-04-09 19:19:36 +0900

    Initial Commit

---
+++ TimeBoxed/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TimeBoxed
+//
+//  Created by ST20638 on 2020/04/09.
+//  Copyright © 2020 Paul Traylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
