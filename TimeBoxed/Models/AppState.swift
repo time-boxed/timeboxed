1a10a62dcc2f404c57b6149360a8fb2d873017ca
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-03-20 21:56:23 +0900

    Move HistoryAction

--- TimeBoxed/Models/AppState.swift
+++ TimeBoxed/Models/AppState.swift
@@ -26,62 +26,6 @@ struct AppState {
     var projects: [Project] = []
 }
 
-enum HistoryAction {
-    case fetch
-    case set(Pomodoro.List)
-    case create(Pomodoro)
-    case update(Pomodoro)
-    case date(id: Int, date: Date)
-    case delete(offset: IndexSet)
-}
-
-extension HistoryAction {
-    func reducer(state: inout AppState, environment: AppEnvironment)
-        -> AnyPublisher<AppAction, Never>?
-    {
-        guard let login = state.login else { return nil }
-        switch self {
-        case .fetch:
-            var request: Request<Pomodoro.List> = login.request(
-                path: "/api/pomodoro", method: .get([]))
-            request.addBasicAuth(login: login)
-            return URLSession.shared.publisher(for: request)
-                .map { HistoryAction.set($0) }
-                .map { AppAction.history($0) }
-                .catch { Just(AppAction.showError(result: $0)) }
-                .eraseToAnyPublisher()
-        case .set(let results):
-            state.pomodoros = results.results.sorted { $0.start > $1.start }
-        case .create(let data):
-            var request: Request<Pomodoro> = login.request(path: "/api/pomodoro", post: data)
-            request.addBasicAuth(login: login)
-            return URLSession.shared.publisher(for: request)
-                .map(mapPomodoro)
-                .catch { Just(AppAction.showError(result: $0)) }
-                .eraseToAnyPublisher()
-        case .update(let pomodoro):
-            var request: Request<Pomodoro> = login.request(
-                path: "/api/pomodoro/\(pomodoro.id)", put: pomodoro)
-            request.addBasicAuth(login: login)
-            return URLSession.shared.publisher(for: request)
-                .map(mapPomodoro)
-                .catch { Just(AppAction.showError(result: $0)) }
-                .eraseToAnyPublisher()
-        case .date(let id, let date):
-            let data = Pomodoro.DateRequest(end: date)
-            var request: Request<Pomodoro> = login.request(path: "/api/pomodoro/\(id)", patch: data)
-            request.addBasicAuth(login: login)
-            return URLSession.shared.publisher(for: request)
-                .map(mapPomodoro)
-                .catch { Just(AppAction.showError(result: $0)) }
-                .eraseToAnyPublisher()
-        case .delete(let offset):
-            print(offset)
-        }
-        return nil
-    }
-}
-
 enum AppAction {
     case login(login: Login, password: String)
     case loginSave(login: Login, password: String)
