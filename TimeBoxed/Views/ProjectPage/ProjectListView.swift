a1bf82e21f681bdd4784d2cb3b75d9ee7251444e
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-03-25 19:43:43 +0900

    Sorting for ProjectListView

--- TimeBoxed/Views/ProjectPage/ProjectListView.swift
+++ TimeBoxed/Views/ProjectPage/ProjectListView.swift
@@ -8,34 +8,59 @@
 
 import SwiftUI
 
+struct ProjectListSorted: View {
+    @EnvironmentObject var store: AppStore
+    var projects: [Project]
+    var body: some View {
+        ForEach(projects) { project in
+            NavigationLink(destination: ProjectDetailView(project: project)) {
+                VStack(alignment: .leading) {
+                    ProjectRowView(project: project)
+                        .font(.title)
+                    DurationView(duration: project.duration)
+                        .font(.footnote)
+                }
+            }
+        }
+        .onDelete(perform: { store.send(.project(.delete(offset: $0))) })
+    }
+
+    init(byDuration: [Project]) {
+        projects = byDuration.sorted { $0.duration > $1.duration }
+    }
+
+    init(byName: [Project]) {
+        projects = byName.sorted { $0.name > $1.name }
+    }
+}
+
 struct ProjectListView: View {
     @EnvironmentObject var store: AppStore
 
     @State private var isPresented = false
     @State private var data = Project.Data()
+    @State private var sorting = Sorting.alphabetic
+
+    enum Sorting {
+        case alphabetic
+        case duration
+    }
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(store.state.projects.sorted { $0.duration > $1.duration }) { project in
-                    NavigationLink(destination: ProjectDetailView(project: project)) {
-                        VStack(alignment: .leading) {
-                            ProjectRowView(project: project)
-                                .font(.title)
-                            DurationView(duration: project.duration)
-                                .font(.footnote)
-                        }
-                    }
-                }
-                .onDelete(perform: { store.send(.project(.delete(offset: $0))) })
-
+                sorting.content(projects: store.state.projects)
             }
             .onAppear(perform: fetch)
             .listStyle(GroupedListStyle())
             .navigationBarTitle("Projects")
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button("Add", action: actionShowAdd)
+                    Picker("Sorting", selection: $sorting) {
+                        Text("Alphabetic").tag(Sorting.alphabetic)
+                        Text("Duration").tag(Sorting.duration)
+                    }.pickerStyle(MenuPickerStyle())
                 }
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button("Reload") {
@@ -76,6 +101,17 @@ struct ProjectListView: View {
 
     private func fetch() {
         store.send(.project(.fetch))
+    }
+}
+
+extension ProjectListView.Sorting {
+    @ViewBuilder func content(projects: [Project]) -> some View {
+        switch self {
+        case .alphabetic:
+            ProjectListSorted(byName: projects)
+        case .duration:
+            ProjectListSorted(byDuration: projects)
+        }
     }
 }
 
